56cba8d65bb71004d264757b28d277b20fe1c39a
GreenhairTurtle <912967642@qq.com>
2022-03-10 17:52:37 +0800

    Update ConstraintLayout.lua

--- Layout/ConstraintLayout.lua
+++ Layout/ConstraintLayout.lua
@@ -8,7 +8,14 @@ PLoop(function()
 
             __base = SpaUI.Widget.LayoutParams
 
-
+            member "StartToStartOf"     { Type = NEString }
+            member "StartToEndOf"       { Type = NEString }
+            member "TopToTopOf"         { Type = NEString }
+            member "TopToBottomOf"      { Type = NEString }
+            member "EndToStartOf"       { Type = NEString }
+            member "EndToEndOf"         { Type = NEString }
+            member "BottomToTopOf"      { Type = NEString }
+            member "BottomToBottomOf"   { Type = NEString }
 
         end)
 
